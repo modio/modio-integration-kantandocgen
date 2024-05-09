8e56129ad77bcea8a3bc72031af7a89def83d1df
Stephen Whittle <swhittle@crypticwarrior.net>
2024-05-09 14:21:56 +1000

    First pass at adding support for blueprint classes and variables

--- Source/KantanDocGen/KantanDocGen.Build.cs
+++ Source/KantanDocGen/KantanDocGen.Build.cs
@@ -41,7 +41,8 @@ public class KantanDocGen : ModuleRules
 				"RenderCore",
 				"SlateRHIRenderer",
 				"Settings",
-				"AssetRegistry"
+				"AssetRegistry",
+				"UMGEditor"
             }
         );
 	}
